897dbc488767f97ff8efee7223e8c75433b8e89e
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-06-07 07:29:44 +0400

    Start monitoring-2

--- start-monioring-1.sh
+++ start-monioring-1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # monitoring/prometheus/docker_build.sh
+export USER_NAME=mzabolotnov
 for i in monitoring/prometheus src/ui src/post-py src/comment; do cd $i; bash docker_build.sh; cd -; done
 # src/comment/docker_build.sh
 # src/post-py/docker_build.sh
 # src/ui/docker_build.sh
 docker-compose -f docker/docker-compose.yml up -d
+docker-compose -f docker/docker-compose-monitoring.yml up -d
