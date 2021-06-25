ba530dd13465ad455ac6991e84611a13bfc43c3b
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-06-25 16:55:59 +0400

    Completed kubernetes-2

--- start_infra.sh
+++ start_infra.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-yc compute instance create \
- --name docker-host \
- --zone ru-central1-a \
- --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
- --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1804-lts,size=15 \
- --ssh-key ~/.ssh/appuser.pub
+yc compute instance create  --name kuber2 \
+--zone ru-central1-a  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
+--create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1804-lts,size=40,type=network-ssd \
+--ssh-key ~/.ssh/appuser.pub  --memory=4  --cores=4
 
  docker-machine create \
  --driver generic \
